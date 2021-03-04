2744d92558412848057c97918d1cd9c296f80f42
David Dugle <david.dugleii@yahoo.com>
2021-03-04 14:04:38 -0500

    refactored search and user to be functional components

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,26 +1,18 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Repos from "../repos/Repos"
 
-export class User extends Component {
+const User = ({ user, loading, getUser, getUserRepos, repos, match}) => {
 
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
 
-    }
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos:PropTypes.func.isRequired,
-        repos: PropTypes.array.isRequired
-    };
-
-    render() {
+    
         const {
            name,
            avatar_url,
@@ -35,9 +27,7 @@ export class User extends Component {
            public_repos,
            public_gists,
            hireable
-        } = this.props.user;
-
-        const { loading, repos } = this.props;
+        } = user;
 
         if (loading) return <Spinner />;
         
@@ -85,7 +75,15 @@ export class User extends Component {
                 <Repos repos={repos} />
             </Fragment>);
         
-    }
+    
 }
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos:PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired
+};
 
 export default User
